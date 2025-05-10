36d0147ae890d37801bca7c94309b522ac7a6705
John Moorman <45500240+mojoro@users.noreply.github.com>
2025-05-10 14:37:05 +0200

    Set up api image pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [imageUrls, setImageUrls] = useState([]);
+
+  async function getImages() {
+    const apiKey = "grGTd5mrcdvzCKeb38qVFsMSYrGbhS6a";
+    const apiUrl = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=cats&limit=10`;
+    try {
+      const response = await fetch(apiUrl);
+      if (!response.ok) {
+        throw new Error(`Http error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const urls = data.data.map((image) => image.images.original.url);
+      setImageUrls(urls);
+    } catch (error) {
+      console.error("Error fetching image: ", error);
+    }
+  }
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
   return (
     <>
       <div className="page-wrapper">
@@ -17,7 +40,7 @@ function App() {
         <main>
           <a href="">
             <div className="memory-card">
-              <img src="" alt="" />
+              <img src={} alt="" />
               <p></p>
             </div>
           </a>
